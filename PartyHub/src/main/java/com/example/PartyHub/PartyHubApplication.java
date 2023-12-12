91d8be79931c5a21232840b69c2b1aebc8b35970
David Vasile <vasiled027@gmail.com>
2023-12-12 17:00:22 +0200

    primul commit

---
+++ PartyHub/src/main/java/com/example/PartyHub/PartyHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.PartyHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyHubApplication.class, args);
+	}
+
+}
