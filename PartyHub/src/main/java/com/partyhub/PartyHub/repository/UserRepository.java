94dc3adca40e4841ab32c1d3deba36ef7d365880
David Vasile <vasiled027@gmail.com>
2023-12-17 18:50:31 +0200

    Rename foler

--- PartyHub/src/main/java/com/partyhub/PartyHub/repository/UserRepository.java
+++ PartyHub/src/main/java/com/partyhub/PartyHub/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package com.example.PartyHub.repository;
+package com.partyhub.PartyHub.repository;
 
-import com.example.PartyHub.entities.User;
+import com.partyhub.PartyHub.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
