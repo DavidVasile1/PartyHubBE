91a09cb7ea3243489f54d580e24097f71fd79102
David Vasile <vasiled027@gmail.com>
2023-12-20 16:36:31 +0200

    ...

--- PartyHub/src/main/java/com/partyhub/PartyHub/controller/AuthController.java
+++ PartyHub/src/main/java/com/partyhub/PartyHub/controller/AuthController.java
@@ -45,6 +45,7 @@ public class AuthController {
                 loginDto.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String token = jwtGenerator.generateToken(authentication);
+        System.out.println(token);
         return new ResponseEntity<>(new AuthResponseDto(token), HttpStatus.OK);
     }
 
--- PartyHub/src/main/java/com/partyhub/PartyHub/security/JwtGenerator.java
+++ PartyHub/src/main/java/com/partyhub/PartyHub/security/JwtGenerator.java
@@ -3,42 +3,50 @@ package com.partyhub.PartyHub.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
 public class JwtGenerator {
 
+    private final SecretKey key = Keys.hmacShaKeyFor(SecurityConstants.JWT_SECRET.getBytes());
 
-    public String generateToken(Authentication authentication){
+    public String generateToken(Authentication authentication) {
         String email = authentication.getName();
         Date currentDate = new Date();
-        Date expireDate = new Date(currentDate.getTime()+SecurityConstants.JWT_EXPIRATION);
+        Date expireDate = new Date(currentDate.getTime() + SecurityConstants.JWT_EXPIRATION);
 
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(currentDate)
                 .setExpiration(expireDate)
-                .signWith(SignatureAlgorithm.HS512, SecurityConstants.JWT_SECRET)
+                .signWith(key, SignatureAlgorithm.HS512)
                 .compact();
     }
 
-    public String getEmailFromJWT(String token){
-        Claims claims = Jwts.parser()
-                .setSigningKey(SecurityConstants.JWT_SECRET)
+    public String getEmailFromJWT(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
         return claims.getSubject();
     }
 
-    public boolean validateToken(String token){
+    public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(SecurityConstants.JWT_SECRET).parseClaimsJws(token);
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
             return true;
-        }catch (Exception ex){
-            throw new AuthenticationCredentialsNotFoundException("JWT was expired and incorrect");
+        } catch (Exception ex) {
+            throw new AuthenticationCredentialsNotFoundException("JWT is invalid or expired");
         }
     }
 }
