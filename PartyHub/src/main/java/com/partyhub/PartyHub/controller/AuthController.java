9e2af8d6a8a84d049a28285663a6607e73a496a8
Aleksandru <mischie.andrey@gmail.com>
2024-01-24 21:45:05 +0200

    email verification, reset password and refactoring

--- PartyHub/src/main/java/com/partyhub/PartyHub/controller/AuthController.java
+++ PartyHub/src/main/java/com/partyhub/PartyHub/controller/AuthController.java
@@ -6,10 +6,11 @@ import com.partyhub.PartyHub.dto.RegisterDto;
 import com.partyhub.PartyHub.entities.Role;
 import com.partyhub.PartyHub.entities.User;
 import com.partyhub.PartyHub.entities.UserDetails;
+import com.partyhub.PartyHub.exceptions.EmailAlreadyUsedException;
+import com.partyhub.PartyHub.exceptions.UserAlreadyVerifiedException;
+import com.partyhub.PartyHub.exceptions.UserNotFoundException;
 import com.partyhub.PartyHub.security.JwtGenerator;
-import com.partyhub.PartyHub.service.RoleService;
-import com.partyhub.PartyHub.service.UserDetailsService;
-import com.partyhub.PartyHub.service.UserService;
+import com.partyhub.PartyHub.service.*;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +25,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -37,6 +40,8 @@ public class AuthController {
     private final PasswordEncoder passwordEncoder;
     private final UserDetailsService userDetailsService;
     private final JwtGenerator jwtGenerator;
+    private final EmailSenderService emailSenderService;
+    private final ProfileService profileService;
 
     @PostMapping(value = "login", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AuthResponseDto> login(@RequestBody LoginDto loginDto){
@@ -46,29 +51,103 @@ public class AuthController {
                 loginDto.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String token = jwtGenerator.generateToken(authentication);
-        System.out.println(token);
+
+        User user = userService.findByEmail(loginDto.getEmail()).orElse(null);
+        if(user!=null){
+            if(!user.isVerified()){
+                AuthResponseDto responseDto = new AuthResponseDto(null);
+                responseDto.setActivated(false);
+                return new ResponseEntity<>(responseDto, HttpStatus.UNAUTHORIZED);
+            }
+        }
         return new ResponseEntity<>(new AuthResponseDto(token), HttpStatus.OK);
+    }
+
+    @GetMapping("/verify/{token}")
+    public ResponseEntity<ApiResponse> verify(@PathVariable UUID token) {
+        Optional<User> optionalUser = this.userService.findByVerificationToken(token);
+        try {
+            if (optionalUser.isPresent()) {
+                User user = optionalUser.get();
+                if (!user.isVerified()) {
+                    user.setVerified(true);
+                    this.userService.save(user);
+                    return ResponseEntity.ok(new ApiResponse(true, "Account activated!"));
+                } else {
+                    throw new UserAlreadyVerifiedException("User already verified!");
+                }
+            } else {
+                throw new UserNotFoundException("User not found!");
+            }
+        }catch ( UserAlreadyVerifiedException e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(false, "User already verified!"));
+        }catch ( UserNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse( false,"User not found!"));
+        }
+    }
+
+    @GetMapping("reset-password/{email}")
+    public ResponseEntity<ApiResponse> sendResetPasswordEmail(@PathVariable String email) {
+        try{
+
+        String clientUrl = "http://localhost:4200";
+
+        User user = userService.findByEmail(email).orElseThrow(()->new UserNotFoundException("User not found!"));
+        user.setVerificationToken(UUID.randomUUID());
+        this.userService.save(user);
+        this.emailSenderService.sendEmail(email, "PartyHub", clientUrl + "/reset-password/" + user.getVerificationToken());
+
+        return new ResponseEntity<>(new ApiResponse(true, "Email sent!"), HttpStatus.OK);
+        }catch (UserNotFoundException e){
+            return new ResponseEntity<>(new ApiResponse(false, "User not found!") ,HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("reset-password/{token}")
+    public ResponseEntity<ApiResponse> resetPassword(@PathVariable UUID token,@RequestBody String newPassword) {
+        try {
+            Optional<User> optionalUser = this.userService.findByVerificationToken(token);
+            User user = optionalUser.orElseThrow(()-> new UserNotFoundException("user not found"));
+            profileService.resetPassword(user.getEmail(), newPassword);
+            return ResponseEntity.ok(new ApiResponse(true, "Password reset successfully!"));
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(false, "User not found!"));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(false, "Failed to reset password"));
+        }
     }
 
     @PostMapping("register")
     @Transactional
-    public ResponseEntity<String> register(@Valid @RequestBody RegisterDto registerDto){
-        if(userService.existsByEmail(registerDto.getEmail())){
-            return new ResponseEntity<>("Email already used!", HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ApiResponse> register(@Valid @RequestBody RegisterDto registerDto){
+        try {
+            String clientUrl = "http://localhost:4200";
+
+            if (userService.existsByEmail(registerDto.getEmail())) {
+                throw new EmailAlreadyUsedException("Email already used!");
+            }
+
+            User user = new User();
+            user.setEmail(registerDto.getEmail());
+            user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+
+            Role role = roleService.findByName("USER").orElseThrow(() -> new RuntimeException("Role not found"));
+
+            user.setRoles(Collections.singletonList(role));
+            user.setVerificationToken(UUID.randomUUID());
+
+            UserDetails userDetails = userDetailsService.create(registerDto.getAge(), registerDto.getFullName());
+            user.setUserDetails(userDetails);
+
+            userService.save(user);
+
+            this.emailSenderService.sendEmail(registerDto.getEmail(), "PartyHub", clientUrl + "/verify/" + user.getVerificationToken());
+
+            return new ResponseEntity<>(new ApiResponse(true, "User registration successful"), HttpStatus.OK);
+        }catch (EmailAlreadyUsedException e){
+            return new ResponseEntity<>(new ApiResponse(false, "Email already used!"), HttpStatus.BAD_REQUEST);
+        }catch (RuntimeException e){
+            return new ResponseEntity<>(new ApiResponse(false, "Role not found!"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        User user = new User();
-        user.setEmail(registerDto.getEmail());
-        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
-        Role role = roleService.findByName("USER").orElseThrow(()-> new RuntimeException("Role not found"));
-        user.setRoles(Collections.singletonList(role));
-
-        UserDetails userDetails = userDetailsService.create(registerDto.getAge(), registerDto.getFullName());
-
-        user.setUserDetails(userDetails);
-
-        userService.save(user);
-
-        return new ResponseEntity<>("User registered successfully!", HttpStatus.OK);
     }
 }
--- PartyHub/src/main/java/com/partyhub/PartyHub/service/UserService.java
+++ PartyHub/src/main/java/com/partyhub/PartyHub/service/UserService.java
@@ -3,9 +3,11 @@ package com.partyhub.PartyHub.service;
 import com.partyhub.PartyHub.entities.User;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserService {
     Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationToken(UUID verificationToken);
     Boolean existsByEmail(String email);
     void save(User user);
     void delete(User user);
