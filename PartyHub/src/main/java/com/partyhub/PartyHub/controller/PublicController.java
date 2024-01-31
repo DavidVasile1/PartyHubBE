9bc9ef3a75cc54bfc8005e359745a47c0f539877
David Vasile <vasiled027@gmail.com>
2024-01-31 19:06:00 +0200

    nimic

--- PartyHub/src/main/java/com/partyhub/PartyHub/controller/PublicController.java
+++ PartyHub/src/main/java/com/partyhub/PartyHub/controller/PublicController.java
@@ -2,17 +2,15 @@ package com.partyhub.PartyHub.controller;
 
 import com.partyhub.PartyHub.dto.EventDto;
 import com.partyhub.PartyHub.entities.Event;
-import com.partyhub.PartyHub.entities.User;
-import com.partyhub.PartyHub.exceptions.UserNotFoundException;
 import com.partyhub.PartyHub.mappers.EventMapper;
 import com.partyhub.PartyHub.service.EventService;
-import com.partyhub.PartyHub.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/public")
 public class PublicController {
 
-    private final UserService userService;
+
     private final EventService eventService;
     private final EventMapper eventMapper;
 
@@ -40,65 +38,8 @@ public class PublicController {
         }
     }
 
-    @GetMapping("/generate-promo-code")
-    public ResponseEntity<String> generatePromoCode() {
-        try {
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            String email = authentication.getName();
-            User user = userService.findByEmail(email)
-                    .orElseThrow(() -> new UserNotFoundException("User not found!"));
-            if (user.getPromoCode() == null || user.getPromoCode().isEmpty()) {
-                userService.generateAndSetPromoCodeForUser(user.getId());
-                return new ResponseEntity<>(user.getPromoCode(), HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>("Promo code already exists", HttpStatus.BAD_REQUEST);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @PutMapping("/edit-promo-code")
-    public ResponseEntity<String> editPromoCode(@RequestBody String newPromoCode) {
-        try {
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            String email = authentication.getName();
-            User user = userService.findByEmail(email).orElseThrow(() -> new UserNotFoundException("User not found!"));
-
-            if (!isValidPromoCode(newPromoCode)) {
-                return new ResponseEntity<>("Invalid promo code format", HttpStatus.BAD_REQUEST);
-            }
-
-            if (userService.isPromoCodeInUse(newPromoCode)) {
-                return new ResponseEntity<>("Promo code already in use", HttpStatus.BAD_REQUEST);
-            }
-
-            user.setPromoCode(newPromoCode);
-            userService.save(user);
-            return new ResponseEntity<>("Promo code updated successfully", HttpStatus.OK);
-        } catch (UserNotFoundException e) {
-            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
 
 
-
-    private boolean isValidPromoCode(String promoCode) {
-        if (promoCode == null || promoCode.length() != 9) {
-            return false;
-        }
-
-        for (int i = 0; i < promoCode.length(); i++) {
-            char ch = promoCode.charAt(i);
-            if (!Character.isLowerCase(ch) && !Character.isDigit(ch)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
 
 
 }
