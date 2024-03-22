74c414fd5eac0b69c026c24a0fda8fc62584440b
David Vasile <vasiled027@gmail.com>
2024-03-22 19:19:47 +0200

    qr code

--- PartyHub/src/main/java/com/partyhub/PartyHub/service/impl/EmailSenderServiceImpl.java
+++ PartyHub/src/main/java/com/partyhub/PartyHub/service/impl/EmailSenderServiceImpl.java
@@ -1,22 +1,34 @@
 package com.partyhub.PartyHub.service.impl;
 
 import com.partyhub.PartyHub.service.EmailSenderService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+
 
 @Service
 @RequiredArgsConstructor
-public class EmailSenderServiceImpl  implements EmailSenderService {
+public class EmailSenderServiceImpl implements EmailSenderService {
     private final JavaMailSender mailSender;
 
+    @Override
     public void sendEmail(String to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
-        mailSender.send(message);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            // true indicates HTML
+            helper.setText(body, true);
+
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Failed to send email", e);
+        }
     }
 }
