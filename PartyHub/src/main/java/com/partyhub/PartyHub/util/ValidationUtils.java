31809ab2deda5a2d087ab3fe4c19a6cb867a5c89
David Vasile <vasiled027@gmail.com>
2024-04-04 20:17:27 +0300

    Tickete cu pret 0 fara datele cardului corect+ refactorizare

---
+++ PartyHub/src/main/java/com/partyhub/PartyHub/util/ValidationUtils.java
@@ -0,0 +1,16 @@
+package com.partyhub.PartyHub.util;
+
+public class ValidationUtils {
+    public static boolean isValidPromoCode(String promoCode) {
+        if (promoCode == null || promoCode.length() != 9) {
+            return false;
+        }
+        for (int i = 0; i < promoCode.length(); i++) {
+            char ch = promoCode.charAt(i);
+            if (!Character.isLowerCase(ch) && !Character.isDigit(ch)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
